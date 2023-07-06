83c8b2aa598b312d0ec2e3e310a4da03f36064ec
lyf <1735842126@qq.com>
2023-07-07 01:04:38 +0800

    book1-5

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -12,20 +12,45 @@ struct Ray {
     dir: Vec3,
 }
 
+#[derive(Copy, Clone)]
+struct Ball {
+    cent: Vec3,
+    radi: f64,
+}
+
+#[derive(Copy, Clone)]
+struct Which {
+    val: f64,
+    num: i32,
+}
+
 fn dot(a: Vec3, b: Vec3) -> f64 {
     a.0 * b.0 + a.1 * b.1 + a.2 * b.2
 }
 
-fn hit_sphere(center: Vec3, radius: f64, r: Ray) -> f64 {
-    let oc = Vec3(r.ori.0 - center.0, r.ori.1 - center.1, r.ori.2 - center.2);
-    let a: f64 = dot(r.dir, r.dir);
-    let b: f64 = 2.0 * dot(oc, r.dir);
-    let c: f64 = dot(oc, oc) - radius * radius;
-    let discriminant: f64 = b * b - 4.0 * a * c;
-    if discriminant < 0.0 {
-        -1.0
+fn hit_sphere(v: Vec<Ball>, r: Ray) -> Which {
+    let mut ans: f64 = 0x10000000 as f64;
+    let mut n: i32 = -1;
+    for (js, i) in (0_i32..).zip(v.iter()) {
+        let center: Vec3 = i.cent;
+        let radius: f64 = i.radi;
+        let oc = Vec3(r.ori.0 - center.0, r.ori.1 - center.1, r.ori.2 - center.2);
+        let a: f64 = dot(r.dir, r.dir);
+        let b: f64 = 2.0 * dot(oc, r.dir);
+        let c: f64 = dot(oc, oc) - radius * radius;
+        let discriminant: f64 = b * b - 4.0 * a * c;
+        if discriminant >= 0.0 {
+            let t: f64 = (-b - discriminant.sqrt()) / (2.0 * a); //注意取近时容易出错
+            if t < ans && t > 0.0 {
+                ans = t;
+                n = js;
+            }
+        }
+    }
+    if n == -1 {
+        Which { val: -1.0, num: 0 }
     } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
+        Which { val: ans, num: n }
     }
 }
 
@@ -37,15 +62,20 @@ impl Ray {
             self.ori.2 + self.dir.2 * t,
         )
     }
-    fn ray_color(&self) -> Vec3 {
+    fn ray_color(&self, v: Vec<Ball>) -> Vec3 {
         let r = Ray {
             ori: self.ori,
             dir: self.dir,
         };
-        let t: f64 = hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, r);
+        let ans: Which = hit_sphere(v.clone(), r);
+        let t: f64 = ans.val;
+        let class: i32 = ans.num;
+        let ball: &Ball = &v[class as usize];
         if t > 0.0 {
             let mut ray = r.at(t);
-            ray.2 += 1.0;
+            ray.0 -= ball.cent.0;
+            ray.1 -= ball.cent.1;
+            ray.2 -= ball.cent.2;
             let length: f64 = dot(ray, ray).sqrt();
             let n = Vec3(ray.0 / length, ray.1 / length, ray.2 / length);
             return Vec3(0.5 * (n.0 + 1.0), 0.5 * (n.1 + 1.0), 0.5 * (n.2 + 1.0));
@@ -62,7 +92,7 @@ impl Ray {
 }
 
 fn main() {
-    let path = std::path::Path::new("output/book1/image4.jpg");
+    let path = std::path::Path::new("output/book1/image5.jpg");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
@@ -70,6 +100,7 @@ fn main() {
     let height = 100;
     let quality = 100;
     let mut img: RgbImage = ImageBuffer::new(width, height);
+    let mut v: Vec<Ball> = Vec::new();
 
     let progress = if option_env!("CI").unwrap_or_default() == "true" {
         ProgressBar::hidden()
@@ -78,17 +109,26 @@ fn main() {
     };
 
     let origin = Vec3(0.0, 0.0, 0.0);
+    let mut b = Ball {
+        cent: Vec3(0.0, 0.0, -1.0),
+        radi: 0.5,
+    };
+    v.push(b);
+    b.cent = Vec3(0.0, -100.5, -1.0);
+    b.radi = 100.0;
+    v.push(b);
+
     for j in (0..height).rev() {
         for i in 0..width {
             let pixel = img.get_pixel_mut(i, height - 1 - j);
             let u = (i as f64) / (width as f64);
-            let v: f64 = (j as f64) / (height as f64);
-            let direction = Vec3(-2.0 + u * 4.0, -1.0 + v * 2.0, -1.0);
+            let w: f64 = (j as f64) / (height as f64);
+            let direction = Vec3(-2.0 + u * 4.0, -1.0 + w * 2.0, -1.0);
             let r = Ray {
                 ori: Vec3(origin.0, origin.1, origin.2),
                 dir: direction,
             };
-            let color = r.ray_color();
+            let color = r.ray_color(v.clone());
             let r: f64 = color.0 * 255.999;
             let g: f64 = color.1 * 255.999;
             let b: f64 = color.2 * 255.999;
